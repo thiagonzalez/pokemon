53d77cdc5c0f7c8cbc78bab60f0360ac72a6120d
Thiago Gonzalez <contato@thiagogonzalez.com>
2018-05-07 16:40:20 -0300

    Use sprite URL for the pokemons to avoid several API hits

--- src/components/PokemonListItem.js
+++ src/components/PokemonListItem.js
@@ -1,37 +1,18 @@
 import React, { Component } from 'react';
-import loader from '../images/loader.svg';
 
 class PokemonListItem extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      images: {
-        front_default: loader
-      }
-    }
-
-    fetch(this.props.pokemon.url)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ 
-          id: data.id,
-          images: data.sprites
-        });
-      });
-  }
-
   render() {
     const pokemon = this.props.pokemon;
-    const pokemonDetails = this.state;
+    const id = pokemon.url.split('/')[6];
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
     return(
       <div className="pokemon-list-item">
-        <a href="">
+        <a href="" data-id={id}>
           <div className="pokemon-image">
-            <img src={pokemonDetails.images.front_default} alt={pokemon.name} />
+            <img src={image} alt={pokemon.name} />
           </div>
-          <span className="pokemon-id">{pokemonDetails.id}</span>
+          <span className="pokemon-id">{`#${id}`}</span>
           <h2 className="pokemon-name">{pokemon.name}</h2>
         </a>
       </div>
